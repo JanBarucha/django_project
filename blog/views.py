4cf967d5a8ffe9cdd0e355bb0ada05ff5a4b996c
JanBarucha <100784471+JanBarucha@users.noreply.github.com>
2022-11-20 00:04:52 +0100

    Added Base Home and About site. Created admin superuser profile. Working on ORM.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author' : 'J_junior',
+        'title' : 'Blog post 1',
+        'content' : 'First post content',
+        'date_posted' : ' August 25, 2022'
+    },
+    {
+        'author': 'J_junior_second_post',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': ' August 27, 2022'
+    }
+]
+
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Blog Homeeeee </h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 
 def about(request):
-    return HttpResponse(
-        '<h1> We are in about view </h1>'
-    )
+    return render(request, 'blog/about.html', { 'title' : 'About' })
