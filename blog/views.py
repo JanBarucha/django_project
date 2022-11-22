a45dcb35648fb9d7ebcc95fceaff14a40fe6bc24
JanBarucha <100784471+JanBarucha@users.noreply.github.com>
2022-11-22 08:44:32 +0100

    Added register page. Added django crispy forms.

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author' : 'J_junior',
-        'title' : 'Blog post 1',
-        'content' : 'First post content',
-        'date_posted' : ' August 25, 2022'
-    },
-    {
-        'author': 'J_junior_second_post',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': ' August 27, 2022'
-    }
-]
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
-
 def about(request):
-    return render(request, 'blog/about.html', { 'title' : 'About' })
+    return render(request, 'blog/about.html', {'title': 'About'})
