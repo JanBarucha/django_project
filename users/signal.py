918a6dba5a002419072a5ae2da7a1544a8f3f90b
JanBarucha <100784471+JanBarucha@users.noreply.github.com>
2022-11-23 22:22:53 +0100

    Added create,update and delete posts.

--- users/signal.py
+++ users/signal.py
@@ -11,7 +11,5 @@ def create_profile(sender, instance, created , **kwargs):
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, created, **kwargs):
-    if not created:
-        Profile.objects.create(user=instance)
     instance.profile.save()
 
